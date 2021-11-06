afd62b847450c591dba01aafcf38d4b11ba9dc7e
anayazia <anayazia06@gmail.com>
2021-11-06 13:28:26 -0500

    Update AutonTimeBased.java
    This is the Auton time based file, it is the main one  where the code will actually run. You need to pull this one along with the Auton Methods in order to avoid errors.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTimeBased.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTimeBased.java
@@ -14,6 +14,8 @@ public class AutonTimeBased extends LinearOpMode{
     private DcMotorEx rightFrontDrive = null;
     private DcMotorEx leftBackDrive = null;
     private DcMotorEx rightBackDrive = null;
+    private DcMotorEx intakeDrive = null;
+    private Servo carouselServo = null;
 
     @Override
     public void runOpMode() {
@@ -22,26 +24,31 @@ public class AutonTimeBased extends LinearOpMode{
         rightFrontDrive = hardwareMap.get(DcMotorEx.class, "right_front");
         leftBackDrive  = hardwareMap.get(DcMotorEx.class, "left_back");
         rightBackDrive = hardwareMap.get(DcMotorEx.class, "right_back");
-
+        intakeDrive = hardwareMap.get(DcMotorEx.class, "intake");
+        carouselServo = hardwareMap.get(Servo.class, "carousel");
         rightBackDrive.setDirection(DcMotorEx.Direction.REVERSE);
         leftBackDrive.setDirection(DcMotorEx.Direction.FORWARD);
         rightFrontDrive.setDirection(DcMotorEx.Direction.FORWARD);
         leftFrontDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        intakeDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        carouselServo.setDirection(Servo.Direction.FORWARD);
 
-        AutonMethods methods = new AutonMethods(this, telemetry, leftFrontDrive,  rightFrontDrive, leftBackDrive, rightBackDrive);
+
+
+        AutonMethods methods = new AutonMethods(this, telemetry, leftFrontDrive,  rightFrontDrive, leftBackDrive, rightBackDrive, intakeDrive, carouselServo);
 
         while (runtime.seconds() < 30) {
-            methods.goForward(2, 1100);
+           methods.goForward(2, 1100);
 
         }
 
 
-//        telemetry.addData("Status", "Run Time: " + runtime.toString());
-//        telemetry.addData("Motors", "left front (%.2f)", leftFrontDrive.getVelocity());
-//        telemetry.addData("Motors", "right front (%.2f)", rightFrontDrive.getVelocity());
-//        telemetry.addData("Motors", "left back (%.2f)", leftBackDrive.getVelocity());
-//        telemetry.addData("Motors", "right back (%.2f)", rightBackDrive.getVelocity());
-//
-//        telemetry.update();
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        telemetry.addData("Motors", "left front (%.2f)", leftFrontDrive.getVelocity());
+        telemetry.addData("Motors", "right front (%.2f)", rightFrontDrive.getVelocity());
+        telemetry.addData("Motors", "left back (%.2f)", leftBackDrive.getVelocity());
+        telemetry.addData("Motors", "right back (%.2f)", rightBackDrive.getVelocity());
+
+        telemetry.update();
     }
 }
