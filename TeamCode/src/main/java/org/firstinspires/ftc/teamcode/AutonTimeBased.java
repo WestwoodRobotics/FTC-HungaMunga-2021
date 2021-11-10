e9d1d44b1fee14e5beb86da971241264b2f88e61
Abraham Lira <abraham51lbucio@gmail.com>
2021-11-09 20:05:21 -0600

    NewestTeleop

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonTimeBased.java
@@ -0,0 +1,23 @@
+//package org.firstinspires.ftc.teamcode;
+//
+//import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+//import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+//import com.qualcomm.robotcore.hardware.DcMotorEx;
+//import com.qualcomm.robotcore.util.ElapsedTime;
+//
+//
+//@Autonomous(name="Abraham: AutonomousTIMEBASED", group="Linear Opmode")
+//public class AutonTimeBased extends LinearOpMode {
+//    private ElapsedTime runtime = new ElapsedTime();
+//    private DcMotorEx leftFrontDrive = null;
+//    private DcMotorEx rightFrontDrive = null;
+//    private DcMotorEx leftBackDrive = null;
+//    private DcMotorEx rightBackDrive = null;
+//
+//    @Override
+//    public void runOpMode() {
+//        while (runtime.seconds() <= 30) {
+//
+//        }
+//    }
+//}
