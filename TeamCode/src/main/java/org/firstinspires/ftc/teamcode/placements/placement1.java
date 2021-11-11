e112e498352ae17cf04dd2f014a5f7cbc3b35f4b
anayazia <anayazia06@gmail.com>
2021-11-11 16:49:33 -0600

    placement 1 for auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/placements/placement1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/placements/placement1.java
@@ -18,6 +18,8 @@ public class placement1 extends LinearOpMode{
     private DcMotorEx rightBackDrive = null;
     private DcMotorEx intakeDrive = null;
     private Servo carouselServo = null;
+    private Servo outtakeServo1 = null;
+    private Servo outtakeServo2 = null;
 
     @Override
     public void runOpMode() {
@@ -28,33 +30,43 @@ public class placement1 extends LinearOpMode{
         rightBackDrive = hardwareMap.get(DcMotorEx.class, "right_back");
         intakeDrive = hardwareMap.get(DcMotorEx.class, "intake");
         carouselServo = hardwareMap.get(Servo.class, "carousel");
-        rightBackDrive.setDirection(DcMotorEx.Direction.FORWARD);
-        leftBackDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        outtakeServo1 = hardwareMap.get(Servo.class, "outtake1");
+        outtakeServo2 = hardwareMap.get(Servo.class, "outtake2");
+
+        rightBackDrive.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBackDrive.setDirection(DcMotorEx.Direction.FORWARD);
         rightFrontDrive.setDirection(DcMotorEx.Direction.FORWARD);
         leftFrontDrive.setDirection(DcMotorEx.Direction.REVERSE);
         intakeDrive.setDirection(DcMotorEx.Direction.REVERSE);
         carouselServo.setDirection(Servo.Direction.FORWARD);
+        outtakeServo1.setDirection(Servo.Direction.FORWARD);
+        outtakeServo2.setDirection(Servo.Direction.REVERSE);
 
 
 
         AutonMethods methods = new AutonMethods(this, telemetry, leftFrontDrive,  rightFrontDrive, leftBackDrive, rightBackDrive, intakeDrive, carouselServo);
 
         while (runtime.seconds() < 30) {
-            methods.rightTurn(.5, 3100);
-            methods.goForward(.2, 3100);
+            methods.rightTurn(.5, 3500 );
+            methods.goForward(.2, 3500);
             methods.turnCarousel(5);
-            methods.rightTurn(1, 3100);
-            methods.leftStrafe(.5, 3100);
-            methods.goForward(5, 3100);
-
+            methods.rightTurn(1, 3500);
+            methods.leftStrafe(1, 3500);
+            methods.goForward(5, 1100);
         }
 
 
+
+
+        //displays the telemetry, don't know if we are using thism
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.addData("Motors", "left front (%.2f)", leftFrontDrive.getVelocity());
         telemetry.addData("Motors", "right front (%.2f)", rightFrontDrive.getVelocity());
         telemetry.addData("Motors", "left back (%.2f)", leftBackDrive.getVelocity());
         telemetry.addData("Motors", "right back (%.2f)", rightBackDrive.getVelocity());
+        telemetry.addData("Boolean", "carousel(%b)", carouselServo);
+        telemetry.addData("boolean", "outtake(%b", outtakeServo1);
+        telemetry.addData("Boolean", "outtake(%b)", outtakeServo2);
 
         telemetry.update();
     }
